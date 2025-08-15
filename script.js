e511960200c46d08dadea7a3ebf77a4a8ab0c74c
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-08-15 14:17:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-//your JS code here. If required.
+const calculate = document.getElementById("calculate");
+calculate.addEventListener("click", function(){
+	const num1 = parseFloat(document.getElementById("num1").value);
+	const num2 = parseFloat(document.getElementById("num2").value);
+	const operator = document.getElementById("operator").value;
+
+	let result;
+
+	switch(operator){
+		case "+":
+			result = num1 + num2;
+			break;
+
+		case "-":
+			result = num1 - num2;
+			break;
+
+		case "*":
+			result = num1 * num2;
+			break;
+
+		case "/":
+			if(num2 === 0){
+				result = "Error: Division by zero";
+			}else{
+				result = num1 / num2;
+			}
+			break;
+			
+		default:
+			result = "Inalid operator"
+	}
+	
+	document.getElementById("result").textContent = "Result = " + result;
+	
+})
+
+const newTexts = ["Bye", "Good Bye", "To", "Your", "Class"];
+
+document.getElementById("change_text").addEventListener("click", function(){
+
+	const divisions = document.querySelectorAll(".division");
+	divisions.forEach((p, index) => {
+		p.textContent = newTexts[index];
+	});
+});
